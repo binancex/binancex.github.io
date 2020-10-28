4c5f7bf968e59e92e23a9242ab888d183e28829a
nance2uiuc <nance2uiuc@gmail.com>
2020-10-28 14:27:13 +0800

    sdf

--- makePage.js
+++ makePage.js
@@ -1,21 +1,21 @@
 function makeModal(project){
-	$('#modalBody').html(`${projects.project.name}`);
-	$('#modalBody').html(`
+	$('#modalTitle').html(`${projects[project].name}`);
+	$('#modalBody').append(`
 	    <div class="justify-content-center py-3" style="text-align: center; padding-top: 20px">
 
 		    <div class="justify-content-center" style="text-align: center; padding: 0 40px">
-		        <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe width="560" height="315" src="https://www.youtube.com/embed/${projects.project.yt}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+		        <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe width="560" height="315" src="https://www.youtube.com/embed/${projects[project].yt}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
 		    </div>
 	    </div>
 
 	    <h5>Project Description</h5>
-	    <p>${projects.project.desc}. See more on our <a href="https://www.${projects.project.site}" target="_blank">website</a></p>
+	    <p>${projects[project].desc}. See more on our <a href="https://www.${projects[project].site}" target="_blank">website</a></p>
 	    <h5>Founding Team</h5>
-	    <p style=" font-style: italic">${projects.project.founders[0].name}, ${projects.project.founders[0].title}</p>
-	    <p>${projects.project.founders[0].bio} Follow me on <a href="https://www.linkedin.com/in/${projects.project.founders[0].linkedin}/" target="_blank">Linkedin</a></p>
-	    <p style=" font-style: italic">${projects.project.founders[1].name}, ${projects.project.founders[1].title}</p>
-	    <p>${projects.project.founders[1].bio} Follow me on <a href="https://www.linkedin.com/in/${projects.project.founders[1].linkedin}/" target="_blank">Linkedin</a></p>
-	`)
+	    <p style=" font-style: italic">${projects[project].founders[0].name}, ${projects[project].founders[0].title}</p>
+	    <p>${projects[project].founders[0].bio} Follow me on <a href="https://www.linkedin.com/in/${projects[project].founders[0].linkedin}/" target="_blank">Linkedin</a></p>
+	    <p style=" font-style: italic">${projects[project].founders[1].name}, ${projects[project].founders[1].title}</p>
+	    <p>${projects[project].founders[1].bio} Follow me on <a href="https://www.linkedin.com/in/${projects[project].founders[1].linkedin}/" target="_blank">Linkedin</a></p>
+	`);
 }
 
 function makeCards(){
@@ -37,7 +37,7 @@ function makeCards(){
 	          </div>
 	        </div>
 	    </div>
-	  	`)
+	  	`);
 	});	
 }
 
