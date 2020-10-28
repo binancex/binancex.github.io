098e62d916c9274e71c0fb24a52b0767318e4c5e
nance2uiuc <nance2uiuc@gmail.com>
2020-10-28 15:45:41 +0800

    l

--- makePage.js
+++ makePage.js
@@ -4,7 +4,11 @@ function makeModal(project){
 	    <div class="justify-content-center py-3" style="text-align: center; padding-top: 20px">
 
 		    <div class="justify-content-center" style="text-align: center; padding: 0 40px">
-		        <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe width="560" height="315" src="https://www.youtube.com/embed/${projects[project].yt}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+		        <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } 
+		        .embed-container iframe, .embed-container object, .embed-container embed 
+		        { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'>
+		        <iframe width="560" height="315" src="https://www.youtube.com/embed/${projects[project].yt}" frameborder="0" 
+		        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
 		    </div>
 	    </div>
 
@@ -13,8 +17,6 @@ function makeModal(project){
 	    <h5>Founding Team</h5>
 	    <p style=" font-style: italic">${projects[project].founders[0].name}, ${projects[project].founders[0].title}</p>
 	    <p>${projects[project].founders[0].bio} Follow me on <a href="https://www.linkedin.com/in/${projects[project].founders[0].linkedin}/" target="_blank">Linkedin</a></p>
-	    <p style=" font-style: italic">${projects[project].founders[1].name}, ${projects[project].founders[1].title}</p>
-	    <p>${projects[project].founders[1].bio} Follow me on <a href="https://www.linkedin.com/in/${projects[project].founders[1].linkedin}/" target="_blank">Linkedin</a></p>
 	`);
 }
 
@@ -31,7 +33,7 @@ function makeCards(){
 	            <h5 class="card-title fellow-card-title">${projects[key].name}</h5>
 	            <p class="card-text fellow-card-text">${projects[key].oneliner}</p>
 	            <div style="text-align: center">
-	              <button class="btn-sm btn-primary binance-button" style="text-align: center; font-size: 12px" data-toggle="modal" data-target="#projectModal" onclick="makeModal("${projects[key]}");">Details
+	              <button class="btn-sm btn-primary binance-button" style="text-align: center; font-size: 12px" data-toggle="modal" data-target="#projectModal" onclick="makeModal('${projects[key]}');">Details
 	              </button>
 	            </div>
 	          </div>
--- projectDB.js
+++ projectDB.js
@@ -37,4 +37,3 @@ var projects =
 			]
 		}
 	}
-;
