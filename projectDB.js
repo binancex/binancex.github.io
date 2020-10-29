788913128c7624efdd4b424c412d14d7be83683c
nance2uiuc <nance2uiuc@gmail.com>
2020-10-29 17:51:29 +0800

    r

--- projectDB.js
+++ projectDB.js
@@ -1,41 +1,22 @@
 var projects = 
 	{
-		/*"Arkane":{
-			"name":"Arkane",
-			"oneliner":"A better blockchain UX is just a few lines of code away!",
-			"yt":"",
-			"desc":"Arkane Network enables game developers to add blockchain assets into their games through APIs. Their solution includes widgets, sandboxes, documentations, test environments, wallets and NFTs as gamers’ collectibles. Arkane Network currently supports more than 9 blockchains and services more than 14 game studios.",
-			"site":"arkane.network",
+		"JustLiquidity":{
+			"name":"JustLiquidity",
+			"oneliner":"JustLiquidity aims to create a Liquidity Protocol mechanism to protect token holders from low liquidity and team dumps by instituting token unlocking schedules in the smart contract.
+",
+			"yt":"UCNKC5b1NWaOGftMmgQ7OYVg",
+			"desc":"There are currently many small cap and new tokens in the market. These are great concepts and projects, but when people go to trade them, there is either no buyer or seller. So there is no liquidity. It’s a great hassle that everyone has experienced at some point or other. The other thing we noticed and that’s something everyone today knows -  is that teams and projects dump their share of tokens. No one likes it. They just want to make a quick buck, and let other hodlers and investors down. We therefore created JustLiquidity as a protocol for SwapLiquidity to solve both these problems at once – there will always be liquidity, and team tokens will be locked. Important to note - this is all 100% decentralized too. Difference Uniswap and JustLiquidity: On Uniswap you need to always provide the main currency which is ETH and the Project Token, but with the JustLiquidity Protocol you just need to provide the Liquidity ‘Only in BNB’ and not ‘BNB and the Token’.  (soon BUSD and ETHb will be available as well). The Liquidity Protocol is the Traders Paradise because it Supports JUL with Liquidity and Protects the Hodlers from Team and Developer Dumps! ",
+			"site":"justliquidity.org",
 			"founders":[
 				{
-					"name": "",
-					"title":"",
-					"bio": "",
-					"linkedin": ""
-				},
-				{
-					"name": "",
-					"title":"",
-					"bio": "",
-					"linkedin": ""
-				}
+					"name": "Tobias Graf",
+					"title":"CEO & Founder",
+					"bio": "My name is Tobias Graf, 30 years young, I am the CEO and Founder of  JustLiquidity.In the first part of my career, I worked for about 11 years as a Finance & Insurance Broker. For two years, that is 2015 and 2016, my company was the biggest insurance and finance seller in the German Health Insurance space. In 2017 I ventured into my own new home in the global Blockchain industry to pioneer new ideas and services.",
+					"linkedin": "tobias-graf-18959512b"
+					}
+				
 			]
-		},
-		"Anyswap":{
-			"name":"Anyswap",
-			"oneliner":"",
-			"yt":"",
-			"desc":"",
-			"site":"",
-			"founders":[
-				{
-					"name": "",
-					"title":"",
-					"bio": "",
-					"linkedin": ""
-				}
-			]
-		}*/
+		}
 
 	}
 
