7a95aad6176a16ae4624bf9c18c8ce5655355690
nance2uiuc <nance2uiuc@gmail.com>
2020-10-29 17:56:09 +0800

    g

--- projectDB.js
+++ projectDB.js
@@ -2,8 +2,7 @@ var projects =
 	{
 		"JustLiquidity":{
 			"name":"JustLiquidity",
-			"oneliner":"JustLiquidity aims to create a Liquidity Protocol mechanism to protect token holders from low liquidity and team dumps by instituting token unlocking schedules in the smart contract.
-",
+			"oneliner":"JustLiquidity aims to create a Liquidity Protocol mechanism to protect token holders from low liquidity and team dumps by instituting token unlocking schedules in the smart contract.",
 			"yt":"UCNKC5b1NWaOGftMmgQ7OYVg",
 			"desc":"There are currently many small cap and new tokens in the market. These are great concepts and projects, but when people go to trade them, there is either no buyer or seller. So there is no liquidity. It’s a great hassle that everyone has experienced at some point or other. The other thing we noticed and that’s something everyone today knows -  is that teams and projects dump their share of tokens. No one likes it. They just want to make a quick buck, and let other hodlers and investors down. We therefore created JustLiquidity as a protocol for SwapLiquidity to solve both these problems at once – there will always be liquidity, and team tokens will be locked. Important to note - this is all 100% decentralized too. Difference Uniswap and JustLiquidity: On Uniswap you need to always provide the main currency which is ETH and the Project Token, but with the JustLiquidity Protocol you just need to provide the Liquidity ‘Only in BNB’ and not ‘BNB and the Token’.  (soon BUSD and ETHb will be available as well). The Liquidity Protocol is the Traders Paradise because it Supports JUL with Liquidity and Protects the Hodlers from Team and Developer Dumps! ",
 			"site":"justliquidity.org",
